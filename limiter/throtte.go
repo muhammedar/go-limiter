57921da23b42a6d7058ec01be0abd7e15e16dc77
Muhammed <muhammed@gabriel.network>
2020-07-27 10:25:32 +0300

    refactoring

--- limiter/throtte.go
+++ limiter/throtte.go
@@ -12,13 +12,20 @@ type Limiter struct {
 	Channel chan time.Time
 }
 
-// BuildLimiter is responsible for building a channel which will be used to store and pass the requests
-func (l *Limiter) BuildLimiter(id string, reqPerSec, bufferSize int) (limiter *Limiter) {
-	limiter.ID = id
+//NewLimiterObject creats a new limiter object
+func NewLimiterObject(id string) *Limiter {
+	return &Limiter{
+		ID:      id,
+		Channel: make(chan time.Time),
+	}
+}
+
+// BuildChannel is responsible for building a channel which will be used to store and pass the requests
+func (l *Limiter) BuildChannel(reqPerSec, bufferSize int) (c chan time.Time) {
 	// building a the buffer using go-channels, and initializing it with time.now
-	limiter.Channel = make(chan time.Time, bufferSize)
+	c = make(chan time.Time, bufferSize)
 	for i := 0; i < bufferSize; i++ {
-		limiter.Channel <- time.Now()
+		c <- time.Now()
 	}
 
 	// ticker will help organizing the time accourding to the needed requests per seconds
@@ -28,18 +35,18 @@ func (l *Limiter) BuildLimiter(id string, reqPerSec, bufferSize int) (limiter *L
 	go func() {
 		for t := range t.C {
 			select {
-			case limiter.Channel <- t: // add the tick to channel
+			case c <- t: // add the tick to channel
 			default: // default will be executed when the channel is already full
 			}
 		}
-		close(limiter.Channel) // close channel when the ticker is stopped
+		close(c) // close channel when the ticker is stopped
 	}()
 
-	return limiter
+	return c
 }
 
 //GetLimiter gets a limiter from a map of limiters
-func (l *Limiter) GetLimiter(limiters map[string]*Limiter, id string) (limiter *Limiter, res bool) {
+func GetLimiter(limiters map[string]*Limiter, id string) (limiter *Limiter, res bool) {
 	limiter, exists := limiters[id]
 
 	if !exists {
